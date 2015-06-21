d9d4027c79855f70ce2c88421a82281371d3f224
Isayes <isayeshu@outlook.com>
2015-06-22 01:50:53 +0800

    处理点击事件

--- app/src/main/java/com/nicerdata/recyclerview/MainActivity.java
+++ app/src/main/java/com/nicerdata/recyclerview/MainActivity.java
@@ -11,6 +11,8 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,33 +29,36 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         //初始化
         initDatas();
         initViews();
-
         mAdapter = new SimpleAdapter(this, mDatas);
         mRecyclerView.setAdapter(mAdapter);
-
         //设置RecyclerView的布局管理
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         mRecyclerView.setLayoutManager(linearLayoutManager);
-
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());//more的动画效果
-//        //设置RecyclerView的Item间的分割线(因为在item_single_textview的布局设置了margin=2dp分隔开了，方便查看效果，所以这里就可以隐藏了)
-//        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
+        //设置RecyclerView的Item间的分割线(因为在item_single_textview的布局设置了margin=2dp分隔开了，方便查看效果，所以这里就可以隐藏了)
+        //mRecyclerView.addItemDecoration(new DividerItemDecoration(this,DividerItemDecoration.VERTICAL_LIST));
+        mAdapter.setOnItemClickListener(new SimpleAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "Click: " + position, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onItemLongClick(View view, int position) {
+                Toast.makeText(MainActivity.this, "LongClick: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     private void initViews() {
-
         mRecyclerView = (RecyclerView) this.findViewById(R.id.id_recyclerView);
     }
 
     private void initDatas() {
-
         //赋值
         mDatas = new ArrayList<String>();
-
         for (int i = 'A'; i <= 'z'; i++) {
             mDatas.add("" + (char) i);
         }
@@ -92,7 +97,7 @@ public class MainActivity extends ActionBarActivity {
                 mRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(5, StaggeredGridLayoutManager.HORIZONTAL));
                 break;
             case R.id.action_staggered:
-                Intent intent = new Intent(this,StaggeredGridLayoutActivity.class);
+                Intent intent = new Intent(this, StaggeredGridLayoutActivity.class);
                 startActivity(intent);
                 break;
             default:
